62b6805377fa20034b12c6c5dfc858789f5a990f
arzzcom <arzz@arzz.com>
2018-08-22 14:14:12 +0900

    savePost 동작변경에 따른 수정

--- templets/webzine/process/savePost.php
+++ templets/webzine/process/savePost.php
@@ -12,33 +12,31 @@
  */
 if (defined('__IM__') == false) exit;
 
-if ($results->success == true) {
-	/**
-	 * 게시물 작성이 완료되면, 편집한 커버이미지를 서버에 저장하고, 게시물 정보를 업데이트한다.
-	 */
-	$cover = Request('cover');
-	$post = $this->getPost($idx);
+/**
+ * 게시물 작성이 완료되면, 편집한 커버이미지를 서버에 저장하고, 게시물 정보를 업데이트한다.
+ */
+$cover = Request('cover');
+$post = $this->getPost($idx);
+
+if ($cover != null && preg_match('/^data:image\/(.*?);base64,(.*?)$/',$cover,$match) == true) {
+	$bytes = base64_decode($match[2]);
+	$path = $this->IM->getModule('attachment')->getTempPath(true).'/'.md5($bytes).'.'.base_convert(microtime(true)*10000,10,32).'.temp';
+	file_put_contents($path,$bytes);
 	
-	if ($cover != null && preg_match('/^data:image\/(.*?);base64,(.*?)$/',$cover,$match) == true) {
-		$bytes = base64_decode($match[2]);
-		$path = $this->IM->getModule('attachment')->getTempPath(true).'/'.md5($bytes).'.'.base_convert(microtime(true)*10000,10,32).'.temp';
-		file_put_contents($path,$bytes);
-		
-		$file = $post->field1 && is_numeric($post->field1) == true ? $this->IM->getModule('attachment')->getFileInfo($post->field1) : null;
-		if ($file != null && $file->module == 'board' && $file->target == 'cover') {
-			$fileIdx = $file->idx;
-			$this->IM->getModule('attachment')->fileReplace($fileIdx,'cover.'.$match[1],$path,true);
-		} else {
-			$fileIdx = $this->IM->getModule('attachment')->fileSave('cover.'.$match[1],$path,'board','cover','PUBLISHED',true);
-		}
-		
-		$this->db()->update($this->table->post,array('image'=>$fileIdx))->where('idx',$idx)->execute();
+	$file = $post->image;
+	if ($file != null && $file->module == 'board' && $file->target == 'cover') {
+		$fileIdx = $file->idx;
+		$this->IM->getModule('attachment')->fileReplace($fileIdx,'cover.'.$match[1],$path,true);
 	} else {
-		$file = $post->field1 && is_numeric($post->field1) == true ? $this->IM->getModule('attachment')->getFileInfo($post->field1) : null;
-		if ($file != null && $file->module == 'board' && $file->target == 'cover') {
-			$this->IM->getModule('attachment')->fileDelete($file->idx);
-		}
-		$this->db()->update($this->table->post,array('image'=>'0'))->where('idx',$idx)->execute();
+		$fileIdx = $this->IM->getModule('attachment')->fileSave('cover.'.$match[1],$path,'board','cover','PUBLISHED',true);
 	}
+	
+	$this->db()->update($this->table->post,array('image'=>$fileIdx))->where('idx',$idx)->execute();
+} else {
+	$file = $post->image;
+	if ($file != null && $file->module == 'board' && $file->target == 'cover') {
+		$this->IM->getModule('attachment')->fileDelete($file->idx);
+	}
+	$this->db()->update($this->table->post,array('image'=>0))->where('idx',$idx)->execute();
 }
 ?>
